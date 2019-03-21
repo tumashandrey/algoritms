22d808dbd735193930433f204f963478f41d81c8
Andrey Tumash <39653460+tumashandrey@users.noreply.github.com>
2019-03-21 22:08:30 +0300

    Update task.rb

--- task.rb
+++ task.rb
@@ -1,31 +1,8 @@
-<<<<<<< HEAD
-def length_of_longest_substring(input_string)
-  result = 0
-  default_index = -1
-  hash_map = Hash.new
-
-  input_string.chars.each_with_index do |str_val, index|
-    if hash_map[str_val] && hash_map[str_val] > default_index
-<<<<<<< HEAD
-	  default_index = index
-	end
-	hash_map[str_val] = index
-	next if result > (index - default_index)  
-	result = index - default_index 	
-=======
-      default_index = index
-    end
-    hash_map[str_val] = index
-    result = index - default_index
->>>>>>> c968f7947a820e01cc532421bd433314965ae5e0
-  end
-  puts "Output: #{result}"
-=======
 def length_of_longest_substring(input_string) 
  result = Array.new
  string = ''
  input_string.chars.each do |value|
-  if string.include?(value)
+   if string.include?(value)
      result << string
      string = value
    else
@@ -34,7 +11,6 @@ def length_of_longest_substring(input_string)
  end
  result << string
  puts "Output: #{result.max{ |a, b| a.length <=> b.length }.size}"
->>>>>>> 91e83764d1b95165207840d8980fba751a92fd75
 end
 length_of_longest_substring("pwwkew")
 
